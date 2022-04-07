450a9220b8f5f2d06278368967358a7569074bff
usrbinkat <usrbinkat@braincraft.io>
2022-04-06 17:02:17 -0700

    update build script
    Signed-off-by: usrbinkat <usrbinkat@braincraft.io>

--- hack/hugo-build.sh
+++ hack/hugo-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 git submodule update --init --recursive
 npm install postcss-cli
-rm -rf docs public
-HUGO_ENV="production" hugo --gc
-mv public docs
+#rm -rf docs public
+HUGO_ENV="production" hugo --gc --cleanDestinationDir --enableGitInfo --destination docs
+#mv public docs
